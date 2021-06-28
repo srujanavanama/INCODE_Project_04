765e6358fa743a62c37091cf7c6bdb77e02fcb48
Ashraf <ashrafunnisa931@gmail.com>
2021-06-29 02:37:21 +1000

    Validate new schedule form
    Add post route for schedule-management

---
+++ public/js/scheduleFormValidation.js
@@ -0,0 +1,76 @@
+// Retrieving the values of form elements 
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("testing form")
+
+    const scheduleForm = document.querySelector("#scheduleForm")
+    const day = document.querySelector("#day")
+    const start_time  = document.querySelector("#start_at")
+    const end_time = document.querySelector("#end_at")
+ 
+    scheduleForm.onsubmit = (e) => { 
+        if (validateDay(day) && validateTimings(start_time, end_time)) {
+             console.log("Form validation is successful")
+             e.preventDefault()
+        } else {
+            console.log("Error while validating the form.")
+            e.preventDefault()
+        }
+    } 
+    
+    function validateDay(day) {
+        if(!day.value === ""){
+            printError("dayErr", "Please select the day");
+            return false
+        } else {
+            printError("dayErr", "");
+            return true
+        }
+    } 
+    
+    function validateTimings(startTime, endTime) {
+      if(validateTime(startTime) && validateTime(endTime) && compareTime(startTime, endTime)) 
+          return true
+       return false
+    }
+
+    function validateTime(time) {
+        if(!time.value === "") {
+            if(time === start_time) {
+                printError("startTimeErr", "Please select the start time");
+                return false
+             } else if(time === end_time) {
+                printError("endTimeErr", "Please select the end time");
+                return false
+             }          
+        } else {
+            if(time === start_time) {
+                printError("startTimeErr", "")
+                return true
+             } else if(time === end_time) {
+                printError("endTimeErr", "")
+                return true
+             }          
+        }  
+    } 
+
+    function compareTime(startTime, endTime) {
+        
+        var startTimeDate= new Date("06/15/2021 "+startTime.value)
+        var endTimeDate= new Date("06/15/2021 "+endTime.value)
+      
+        if(startTimeDate >= endTimeDate ){
+            printError("endTimeErr","End time can't be less than the start time")
+            console.log("Start time is greater than the end time")
+            return false
+        }else{
+            printError("endTimeErr", "");
+            console.log("End time is greater than the start time")
+            return true
+        }
+    } 
+
+     // Defining a function to display error message
+    function printError(elemId, hintMsg) {
+         document.getElementById(elemId).innerHTML = hintMsg;
+    }      
+})
--- routes/schedulesManagementRouter.js
+++ routes/schedulesManagementRouter.js
@@ -3,12 +3,14 @@ const router = express.Router()
 const db = require('../database')
 const { redirectToLogin } = require('../middleware')
 
+//Display all existing schedules of the user
 // router.get('/', redirectToLogin, (req, res) => {
 router.get('/', (req, res) => {
   //console.log("user id: " + req.session.userId)
-  const uid = 3
+  const uid = 1
   //#TODO: Replace uid with req.session.userId
-  db.any('SELECT id, day, TO_CHAR(start_at, \'fmHH12:MI AM\') as start_at, TO_CHAR(end_at, \'fmHH12:MI AM\') as end_at FROM schedules WHERE user_id = $1;', [uid])
+ 
+  db.any('SELECT u.firstname, u.lastname, s.id, s.day, TO_CHAR(s.start_at, \'fmHH12:MI AM\') as start_at, TO_CHAR(s.end_at, \'fmHH12:MI AM\') as end_at FROM users u INNER JOIN schedules s ON u.user_id=s.user_id WHERE u.user_id = $1  ORDER BY day;', [uid])
   .then((result) => {
     res.render('pages/schedulesManagement', {
         schedules: result
@@ -22,32 +24,29 @@ router.get('/', (req, res) => {
     })
 })
 
- // Save schedule to the database
-  // router.post('/', (req, res) => {
-  //   const user_id = 3
+ // Save new schedule to the database
+  router.post('/', (req, res) => {
+    const user_id = 1
     //#TODO: Replace uid with req.session.userId
-  //   console.log("start_at: " + req.body.start_at)
-  //   console.log("end_at: " + req.body.end_at)
+    console.log("start_at: " + req.body.start_at)
+    console.log("end_at: " + req.body.end_at)
 
-  //   // let current_date = new Date()
-  //   // let current_date = new Date("" + req.body.start_at)
-  //   // console.log("current date: " + current_date)
+     let current_date = new Date()       // Returns Tue Dec 12 2017 11:18:30 GMT+0530 (IST) {}
+     console.log("current date: " + current_date)
 
-  //   // req.body.start_at = new Date( req.body.start_at)
-  //   // req.body.end_at = new Date(req.body.end_at)
-  //   db.none('INSERT INTO schedules (user_id, day, start_at, end_at) VALUES ($1, $2, $3, $4);', 
-  //       [Number(user_id), Number(req.body.day), req.body.start_at, req.body.end_at])
-  //   //db.none('INSERT INTO schedules (user_id, day, start_at, end_at) VALUES ($1, $2, $3, $4);', 
-  //       //[Number(req.session.user_id), Number(req.body.day), req.body.start_at, req.body.end_at])        
-  //   .then(() => {
-  //        res.redirect(req.get('referer'))        //reloads the current page
-  //   })
-  //   .catch((err) => {
-  //         res.render("pages/error", {
-  //              message:err.message + err.query
-  //         });
-  //   })
-  // })
+    db.none('INSERT INTO schedules (user_id, day, start_at, end_at) VALUES ($1, $2, $3, $4);', 
+        [Number(user_id), Number(req.body.day), "2021/06/29 08:30:00", "2021/06/29 05:30:00"])
+    //db.none('INSERT INTO schedules (user_id, day, start_at, end_at) VALUES ($1, $2, $3, $4);', 
+        //[Number(req.session.user_id), Number(req.body.day), req.body.start_at, req.body.end_at])        
+    .then(() => {
+         res.redirect(req.get('referer'))        //reloads the current page
+    })
+    .catch((err) => {
+          res.render("pages/error", {
+               message:err.message + err.query
+          });
+    })
+  })
 
 module.exports = router
 
