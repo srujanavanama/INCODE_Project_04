4ba9285999546619c7435b3d38a5a0762d96cbbb
Ashraf <ashrafunnisa931@gmail.com>
2021-07-04 04:39:17 +1000

    Added README.md file

--- routes/schedulesManagementRouter.js
+++ routes/schedulesManagementRouter.js
@@ -5,8 +5,6 @@ const { redirectToLogin } = require('../middleware')
 
 //Display all existing schedules of the user
  router.get('/', redirectToLogin, (req, res) => {
-  console.log("user id: " + req.session.userId)
-  //const uid = 4
 db.any('SELECT u.firstname, u.lastname, s.id, s.day, TO_CHAR(s.start_at, \'fmHH12:MI AM\') as start_at, TO_CHAR(s.end_at, \'fmHH12:MI AM\') as end_at FROM users u INNER JOIN schedules s ON u.user_id=s.user_id WHERE u.user_id = $1  ORDER BY day;', [req.session.userId])
 .then((result) => {
   console.log("Result: " + result)
@@ -30,11 +28,6 @@ db.any('SELECT u.firstname, u.lastname, s.id, s.day, TO_CHAR(s.start_at, \'fmHH1
 
  // Save new schedule to the database
   router.post('/', redirectToLogin, (req, res) => {
-
-    console.log("start_at: " + req.body.start_at)
-    console.log("end_at: " + req.body.end_at)
-    console.log("body: " + req.body)
-    //const uid = 4
     db.none('INSERT INTO schedules (user_id, day, start_at, end_at) VALUES ($1, $2, TO_TIMESTAMP($3, $4), TO_TIMESTAMP($5, $6));', 
         [req.session.userId, Number(req.body.day), req.body.start_at, "HH24:MI", req.body.end_at, "HH24:MI"])        
     .then(() => {
